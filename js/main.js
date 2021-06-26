6cbd962558e0a9eecfaf47930abeb92660ecfe78
phamgiang0167 <jagsiph@gmail.com>
2021-06-26 11:53:13 +0700

    change code

--- js/main.js
+++ js/main.js
@@ -1,119 +1,64 @@
 document.getElementById('calculate').addEventListener('click', function(){
     action(this.id)
-    
 })
 document.getElementById('printBill').addEventListener('click', function(){
     action(this.id)
 })
 
 function action(action){
-    var type = getGrabType()
-    var km = getDistance()
-    var waiting = getWaitingTime()
-    if(!type || !km || !waiting){
+    let typeGrab = getGrabType()
+    let distance = getDistance()
+    let waitingTime = getWaitingTime()
+    let price = {
+        "grabCar": {
+            "first": 8000,
+            "second": 7500,
+            "third":7000,
+            "waiting": 2000
+        },
+        "grabSUV": {
+            "first": 9000,
+            "second": 8500,
+            "third":8000,
+            "waiting": 3000
+        },
+        "grabBlack": {
+            "first": 10000,
+            "second": 9500,
+            "third":9000,
+            "waiting": 3500
+        },
+    }
+    if(!typeGrab || !distance || !waitingTime){
         alert("Một vài trường thiếu hoặc không hợp lệ!!!")
+        document.getElementById('divThanhTien').style.display = 'none'
+        document.getElementById('bill').style.display = 'none'
         return
     }
+
+    let bill = new Bill(price[typeGrab].first, price[typeGrab].second, price[typeGrab].third, price[typeGrab].waiting, distance, waitingTime)
     if(action == "calculate"){
-        calculatePrice(type, km, waiting)
+        document.getElementById('divThanhTien').style.display = "block"
+        document.getElementById('xuatTien').innerHTML = bill.getTotalPrice().toString()
     }else{
-        createBill(type, km, waiting)
+        let billDetail = bill.getBillDetail()
+        for (var key in billDetail) {
+            if(key == "waiting"){
+                fillingBill(key, billDetail[key].waitingTime, billDetail[key].price, price[typeGrab][key])
+            }else if(key != "totalPrice"){
+                fillingBill(key, billDetail[key].distance, billDetail[key].price, price[typeGrab][key])
+            }else{
+                document.getElementById('total').innerHTML = billDetail[key]
+            }   
+        }
+        document.getElementById('bill').style.display = "table"
     }
 }
 
-
-/** 
- * _1: 0 to 1
- * _2: 1 to 19
- * _3: more than 19
- * */ 
-
-var price = {
-    "grabCar": {
-        "_1": 8000,
-        "_2": 7500,
-        "_3":7000,
-        "_waiting": 2000
-    },
-    "grabSUV": {
-        "_1": 9000,
-        "_2": 8500,
-        "_3":8000,
-        "_waiting": 3000
-    },
-    "grabBlack": {
-        "_1": 10000,
-        "_2": 9500,
-        "_3":9000,
-        "_waiting": 3500
-    },
-}
-
-function calculatePrice(typeGrab, distance, waitingTime){
-    var total = 0
-    if(distance >=0 && distance <=1){
-        total = price[typeGrab]._1*distance
-    }else if(distance > 1 && distance <= 19){
-        total = price[typeGrab]._1 + price[typeGrab]._2*(distance - 1)
-    }else{
-        total = price[typeGrab]._1 + price[typeGrab]._2*18 + price[typeGrab]._2*(distance - 19)
-    }
-
-    if(waitingTime > 3){
-        total += Math.round(waitingTime/3)*price[typeGrab]._waiting
-    }
-    document.getElementById('divThanhTien').style.display = 'block'
-    document.getElementById('xuatTien').innerHTML = total
-}
-
-function fillingBill(stage, typeGrab, total, distance){
-    document.getElementById("stage" + stage).innerHTML = distance
-    document.getElementById("price" + stage).innerHTML = price[typeGrab][stage]
-    document.getElementById("total" + stage).innerHTML = total
-}
-
-function createBill(typeGrab, distance, waitingTime){
-    var total_3 = 0 // total price (more than 19km)
-    var total_2 = 0 // total price (1km to 19km)
-    var total_1 = 0 // total price (0km to 1km)
-    var total_waiting = 0 // total price for waitting time
-
-    // price details for distance over 19km
-    if(distance > 19){
-        total_3 = (distance - 19)*price[typeGrab]._3
-        fillingBill('_3', typeGrab, total_3, distance - 19)
-        distance = 19
-    }else{
-        fillingBill('_3', typeGrab, 0, 0)
-    }
-
-    // price details for distance from 1km to 19km
-    if(distance > 1 && distance <= 19){
-        total_2 = (distance - 1)*price[typeGrab]._2
-        fillingBill('_2', typeGrab, total_2, distance - 1)
-        distance = 1
-    }else{
-        fillingBill('_2', typeGrab, 0, 0)
-    }
-
-    // price details for distance from 0km to 1km
-    if(distance > 0 && distance <= 1){
-        total_1 = distance*price[typeGrab]._1
-        fillingBill('_1', typeGrab, total_1, distance)
-    }else{
-        fillingBill('_1', typeGrab, 0, 0)
-    }
-
-    // price details for waiting time
-    if(waitingTime > 3){
-        total_waiting = Math.round(waitingTime/3)*price[typeGrab]._waiting
-        fillingBill('_waiting', typeGrab, total_waiting, waitingTime)   
-    }else{
-        fillingBill('_waiting', typeGrab, 0, waitingTime)   
-    }
-    
-    document.getElementById('total').innerHTML = "TỔNG TIỀN: " + parseInt(total_1 + total_2 + total_3 + total_waiting)
-    document.getElementById('bill').style.display = 'table'
+function fillingBill(stage, number, price, priceGrab){
+    document.getElementById("stage_" + stage).innerHTML = number
+    document.getElementById("price_" + stage).innerHTML = priceGrab
+    document.getElementById("total_" + stage).innerHTML = price
 }
 
 function getGrabType(){
@@ -146,3 +91,65 @@ function getWaitingTime(){
 function isNumber(n) { 
     return /^-?[\d.]+(?:e-?\d+)?$/.test(n); 
 } 
+
+
+function Bill(priceFirst, priceSecond, priceThird, priceWaitingTime, totalDistance, waitingTime){
+    let billDetail = function(){
+        let billDetail = {
+            first: {},
+            second: {},
+            third: {},
+            waiting: {},
+            totalPrice: 0
+        }
+
+        let distance = totalDistance
+        // price details for distance over 19km
+        if(distance > 19){
+            billDetail.third.price = (distance - 19)*priceThird
+            billDetail.third.distance = distance - 19
+            distance = 19
+        }else{
+            billDetail.third.price = 0
+            billDetail.third.distance = 0
+        }
+    
+        // price details for distance from 1km to 19km
+        if(distance  > 1 && distance  <= 19){
+            billDetail.second.price = (distance - 1)*priceSecond
+            billDetail.second.distance= distance - 1
+            distance = 1
+        }else{
+            billDetail.second.price = 0
+            billDetail.second.distance = 0
+        }
+    
+        // price details for distance from 0km to 1km
+        if(distance > 0 && distance <= 1){
+            billDetail.first.price= distance*priceFirst
+            billDetail.first.distance = distance
+        }
+    
+        // price details for waiting time
+        if(waitingTime > 3){
+            billDetail.waiting.price = Math.floor(waitingTime/3)*priceWaitingTime
+            billDetail.waiting.waitingTime = waitingTime
+        }
+
+        billDetail.totalPrice = billDetail.first.price + 
+                                billDetail.second.price + 
+                                billDetail.third.price +
+                                billDetail.waiting.price
+        return billDetail
+    }
+
+    let totalPrice = billDetail().totalPrice
+
+    this.getBillDetail = function(){
+        return billDetail()
+    }
+
+    this.getTotalPrice = function(){
+        return totalPrice
+    }
+}
